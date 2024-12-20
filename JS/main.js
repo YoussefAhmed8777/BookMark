678487e84f0b09fe1c0920ca8bc7801bdb01d6d5
YouusefAhmed8777 <youssefahmedamin28@gmail.com>
2024-12-20 13:29:28 +0200

    Added new features ( validation, prevent duplication, and active alert )

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,9 @@
 let webNameInput = document.getElementById('webName');
 let webUrlInput = document.getElementById('webUrl');
+let selectedInput = document.querySelectorAll('.selectedInput');
 
-let regex1 = /^[a-zA-Z0-9 ]{2,}$/;
-let regex2 = / ^(ftp|http|https):\/\/[^ "]+$ /;
+let regex1 = /^[a-zA-Z0-9 ]{3,32}$/i; // this (i) in the end allows it to accept the value either it's capital or small.
+let regex2 = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)$/;
 
 let websitesBox = []; 
 
@@ -15,11 +16,24 @@ function addWebsite(){
     name:webNameInput.value,
     url:webUrlInput.value,
   };
+  if(validation(webNameInput.id, webNameInput.value)&& validation(webUrlInput.id, webUrlInput.value)&& preventDuplication(websitesBox, website)){
+    Swal.fire({
+      icon: "success",
+      title: "Congrats",
+      text: "It's Working"
+    });
     websitesBox.push(website);
     displayWebsite();
     clearInput();
     localStorage.setItem("allWebsites", JSON.stringify(websitesBox));
-} 
+  }else{
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!"
+    });
+  };
+}; 
 
 function displayWebsite(){
   var cartona='';
@@ -41,6 +55,8 @@ function displayWebsite(){
 function clearInput(){
   webNameInput.value="";
   webUrlInput.value="";
+  webNameInput.classList.remove('is-valid');
+  webUrlInput.classList.remove('is-valid');
 } 
 
 function deleteWebsite(index){
@@ -74,3 +90,42 @@ function updateWebsite(){
   document.getElementById('update-btn').style.display="none";
   document.getElementById('add-btn').style.display="block";
 }
+
+for (let i=0; i<selectedInput.length; i++){
+  selectedInput[i].addEventListener(('input'),function(e){
+    let inputId= e.target.id;
+    let inputValue= e.target.value;
+    validation(inputId, inputValue);
+  })
+};
+
+function validation(id,value){
+  let objRegex= {
+    webName:regex1,
+    webUrl:regex2
+  };
+
+  let element=document.getElementById(id);
+  let errorMessage=document.getElementById(id+'Error');
+
+  if (objRegex[id].test(value)==true){
+   element.classList.add('is-valid');
+   element.classList.remove('is-invalid');
+   errorMessage.innerHTML='';
+   return true;
+  }else{
+    element.classList.add('is-invalid');
+    element.classList.remove('is-valid');
+    errorMessage.innerHTML=(id=='webName')?'Website name must at least contain 3 characters':'Wesite URL must be a valid one';
+    return false;
+  };
+};
+
+function preventDuplication(arr,newObj){
+  for (let i= 0; i< arr.length; i++) {
+    if(arr[i].webName==newObj.webName || arr[i].webUrl==newObj.webUrl){
+      return false;
+    };
+  };
+  return true;
+};
